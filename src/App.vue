a05db431f30d0c1e06758ee5d948983db6c926bf
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-12-22 09:44:37 +0100

    Add markers

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,45 @@
 <script>
 import gmapsInit from './utils/gmaps';
 
+const locations = [
+  {
+    position: {
+      lat: 48.160910,
+      lng: 16.383330,
+    },
+  },
+  {
+    position: {
+      lat: 48.174270,
+      lng: 16.329620,
+    },
+  },
+  {
+    position: {
+      lat: 48.146140,
+      lng: 16.297030,
+    },
+  },
+  {
+    position: {
+      lat: 48.135830,
+      lng: 16.194460,
+    },
+  },
+  {
+    position: {
+      lat: 48.306091,
+      lng: 14.286440,
+    },
+  },
+  {
+    position: {
+      lat: 47.503040,
+      lng: 9.747070,
+    },
+  },
+];
+
 export default {
   name: `App`,
   async mounted() {
@@ -21,6 +60,9 @@ export default {
         map.setCenter(results[0].geometry.location);
         map.fitBounds(results[0].geometry.viewport);
       });
+
+      const markers = locations
+        .map(x => new google.maps.Marker({ ...x, map }));
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error);
